0fcbe2ac3964dbc70c7254960b52d69fe60eed14
Timo Glastra <timo@animo.id>
2024-02-06 15:17:17 +0700

    set default update config
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/storage/migration/UpdateAssistant.ts
+++ packages/core/src/storage/migration/UpdateAssistant.ts
@@ -11,7 +11,7 @@ import { WalletError } from '../../wallet/error/WalletError'
 
 import { StorageUpdateService } from './StorageUpdateService'
 import { StorageUpdateError } from './error/StorageUpdateError'
-import { CURRENT_FRAMEWORK_STORAGE_VERSION, supportedUpdates } from './updates'
+import { DEFAULT_UPDATE_CONFIG, CURRENT_FRAMEWORK_STORAGE_VERSION, supportedUpdates } from './updates'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class UpdateAssistant<Agent extends BaseAgent<any> = BaseAgent> {
@@ -20,7 +20,7 @@ export class UpdateAssistant<Agent extends BaseAgent<any> = BaseAgent> {
   private updateConfig: UpdateConfig
   private fileSystem: FileSystem
 
-  public constructor(agent: Agent, updateConfig: UpdateConfig) {
+  public constructor(agent: Agent, updateConfig: UpdateConfig = DEFAULT_UPDATE_CONFIG) {
     this.agent = agent
     this.updateConfig = updateConfig
 
